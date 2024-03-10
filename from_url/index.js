a30e58b7781f076984bcff9b5d788229ada59b89
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-03-09 22:49:15 -0600

    test

--- from_url/index.js
+++ from_url/index.js
@@ -1,5 +1,6 @@
-fetch(prompt("url")).then(v => v.text().then(v => {
+(() => {fetch(prompt("url")).then(v => v.text().then(v => {
     extensions[crypto.randomUUID()] = v;
     delete extensions.from_url;
     (new Function(v))();
 }))
+})()
