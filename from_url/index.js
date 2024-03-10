80a1b16f9a09a5d9df297bc1202953b302064ed7
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-03-09 22:40:46 -0600

    test

--- from_url/index.js
+++ from_url/index.js
@@ -1,7 +1,7 @@
-// (async () => {
-//     const content = await (await fetch(prompt("url of extension"))).text();
-//     const id = window.crypto.randomUUID();
-//     extensions[id] = content;
-//     delete extensions.from_url;
-//     (new Function(content))();
-// })()
+(async () => {
+    const content = await (await fetch(prompt("url of extension"))).text();
+    const id = window.crypto.randomUUID();
+    extensions[id] = content;
+    delete extensions.from_url;
+    (new Function(content))();
+})()
