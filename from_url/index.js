fda386eee6a580ed7dbe582665295442e4cf42a2
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-03-09 20:34:06 -0600

    added extension

--- from_url/index.js
+++ from_url/index.js
@@ -1,1 +1,32 @@
 "use strict";
+class testExtension {
+    constructor() {
+        this.generator = {
+            testBlock(block) {
+                return block.getField("Num");
+                ;
+            }
+        };
+    }
+    Info() {
+        return {
+            name: "test extension",
+            color: "#aaa",
+            ID: "test-extension",
+            blocks: [
+                {
+                    opcode: "testBlock",
+                    color: 0,
+                    blockType: Penguin.blockType.Value("Number"),
+                    args: [
+                        Penguin.Argument.Dummy([
+                            Penguin.Field.Text("test"),
+                            Penguin.Field.NumberInput("Num", 0),
+                        ]),
+                    ]
+                }
+            ],
+        };
+    }
+}
+Penguin.LoadExtension(testExtension);
