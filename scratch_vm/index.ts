3056b52c574ecf086b14d439d95b97952ad95e0a
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-07-02 02:21:09 -0500

    added test block

--- scratch_vm/index.ts
+++ scratch_vm/index.ts
@@ -5,7 +5,19 @@
         name: "Scratch Vm",
         color: "#0000FF",
         ID: "Scratch_VM",
-        blocks: [],
+        blocks: [
+            {
+                opcode: "test",
+                color: 225,
+                blockType: Penguin.blockType.Statement(),
+                args: [
+                    Penguin.Argument.Dummy([
+                        Penguin.Field.Text("test"),
+                        Penguin.Field.TextInput("test", "test")
+                    ])
+                ]
+            }
+        ],
       };
     }
     generator = {};
