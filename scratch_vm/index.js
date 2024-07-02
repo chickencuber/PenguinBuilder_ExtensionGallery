8e084fa4adc843781eccdb7ecd94a296f152065e
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-07-02 02:26:11 -0500

    transpiled typescript

--- scratch_vm/index.js
+++ scratch_vm/index.js
@@ -1,1 +1,33 @@
 "use strict";
+(() => {
+    class Extension {
+        constructor() {
+            this.generator = {
+                test(block) {
+                    return `//${block.getField("test")}`;
+                }
+            };
+        }
+        Info() {
+            return {
+                name: "Scratch Vm",
+                color: "#0000FF",
+                ID: "Scratch_VM",
+                blocks: [
+                    {
+                        opcode: "test",
+                        color: 225,
+                        blockType: Penguin.blockType.Statement(),
+                        args: [
+                            Penguin.Argument.Dummy([
+                                Penguin.Field.Text("test"),
+                                Penguin.Field.TextInput("test", "test")
+                            ])
+                        ]
+                    }
+                ],
+            };
+        }
+    }
+    Penguin.LoadExtension(Extension);
+})();
