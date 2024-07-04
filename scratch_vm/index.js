3833f8e36dc02f5fdcc0afa63e3f49a7ba09e2ef
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-07-04 01:01:48 -0500

    another fix

--- scratch_vm/index.js
+++ scratch_vm/index.js
@@ -40,12 +40,13 @@
                         color: 225,
                         blockType: Penguin.blockType.Value("Number"),
                         args: [
-                            Penguin.Argument.Value("Sprite", "Sprite", [Penguin.Field.Text("Get"),
+                            Penguin.Argument.Value("Sprite", "Sprite", [
+                                Penguin.Field.Text("Get"),
                                 Penguin.Field.MenuInput("Part", {
                                     X: "x",
                                     Y: "y",
                                 }),
-                                Penguin.Field.Text("of")
+                                Penguin.Field.Text("of"),
                             ]),
                         ],
                     },
--- scratch_vm/index.ts
+++ scratch_vm/index.ts
@@ -21,12 +21,13 @@
             color: 225,
             blockType: Penguin.blockType.Value("Number"),
             args: [
-              Penguin.Argument.Value("Sprite", "Sprite", [Penguin.Field.Text("Get"),
+              Penguin.Argument.Value("Sprite", "Sprite", [
+                Penguin.Field.Text("Get"),
                 Penguin.Field.MenuInput("Part", {
                   X: "x",
                   Y: "y",
                 }),
-                Penguin.Field.Text("of")
+                Penguin.Field.Text("of"),
               ]),
             ],
           },
