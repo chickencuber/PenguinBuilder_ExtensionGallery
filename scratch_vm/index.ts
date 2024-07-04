88509dcebd60d432060572b453541e3d4f9a6e21
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-07-04 00:55:09 -0500

    another fix

--- scratch_vm/index.ts
+++ scratch_vm/index.ts
@@ -22,14 +22,14 @@
             color: 225,
             blockType: Penguin.blockType.Value("Number"),
             args: [
-              Penguin.Argument.Dummy([Penguin.Field.Text("Get")]),
-              Penguin.Argument.Value("Sprite", "Sprite", []),
-              Penguin.Argument.Dummy([
+              Penguin.Argument.Dummy([Penguin.Field.Text("Get"),
                 Penguin.Field.MenuInput("Part", {
                   X: "x",
                   Y: "y",
                 }),
+                Penguin.Field.Text("of")
               ]),
+              Penguin.Argument.Value("Sprite", "Sprite", []),
             ],
           },
         ],
